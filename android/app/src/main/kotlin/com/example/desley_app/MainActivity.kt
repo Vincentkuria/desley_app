2a677e3a91815f5756eb715092c54105e14175e9
vini <vinvincent254@gmail.com>
2024-03-08 22:45:28 +0300

    started

---
+++ android/app/src/main/kotlin/com/example/desley_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.desley_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [Colors.indigo, Colors.lightBlue]),
+        ),
+        child: Center(
+          child: Image.asset('assets/images/logo.png', width: 150, height: 150),
+        ),
+      ),
+    );
+  }
+}
